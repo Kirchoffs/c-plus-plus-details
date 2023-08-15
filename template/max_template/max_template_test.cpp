7c20adaf4db5408ea29adbeaa7a1fafcec55a77b
Yonghao Shu <123shuyonghao@gmail.com>
2023-08-14 21:13:44 -0700

    Max template demo with different types of arguments

--- template/max_template/max_template_test.cpp
+++ template/max_template/max_template_test.cpp
@@ -15,4 +15,8 @@ int main()
     std::string s1 = "mathematics";
     std::string s2 = "physics";
     std::cout << "max(s1, s2): " << ::max(s1, s2) << std::endl;
+
+    int o1 = 42;
+    double o2 = 2.718;
+    std::cout << "max_max_decltype(o1, o2): " << ::max_decltype(o1, o2) << std::endl;
 }
--- template/max_template/max_template_test.hpp
+++ template/max_template/max_template_test.hpp
@@ -1,6 +1,14 @@
+#include <type_traits>
+
 template<typename T>
 
 T max(T a, T b) 
 {
     return a > b ? a : b;
+}
+
+template<typename T, typename U>
+auto max_decltype(T a, U b) -> typename std::decay<decltype(a > b ? a : b)>::type
+{
+    return b < a ? a : b;
 }
